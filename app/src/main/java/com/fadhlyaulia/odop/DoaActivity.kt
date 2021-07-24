61bea293fd23a0a5cf9baf03f58e7e2570186982
fdlrswlly <fadhly.aulia123@gmail.com>
2021-07-24 13:54:49 +0700

    Update Baru

--- app/src/main/java/com/fadhlyaulia/odop/DoaActivity.kt
+++ app/src/main/java/com/fadhlyaulia/odop/DoaActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.content.Intent
 import android.provider.Telephony
+import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.cardview.widget.CardView
@@ -11,15 +12,21 @@ import androidx.cardview.widget.CardView
 class DoaActivity : AppCompatActivity() {
     private lateinit var rvDoa: RecyclerView
     private var list: ArrayList<Doa> = arrayListOf()
+    lateinit var btnex : View
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_doa)
-
+        btnex = findViewById(R.id.btn_doa_exit)
         rvDoa = findViewById(R.id.rv_doa)
         rvDoa.setHasFixedSize(true)
 
         list.addAll(DoaData.listData)
         showRecyclerViewListMode()
+
+        btnex.setOnClickListener {
+            val pindah = Intent(this, MainActivity::class.java)
+            startActivity(pindah)
+        }
 
     }
 
@@ -28,4 +35,5 @@ class DoaActivity : AppCompatActivity() {
         val doaAdapter = DoaAdapter(list)
         rvDoa.adapter = doaAdapter
     }
+
 }
